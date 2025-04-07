7ec1dd79a380466702060b43bb0ce5dd7c855b09
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-04-07 14:15:43 +0800

    add_record_table

---
+++ app/Models/Record.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Record extends Model
+{
+    protected $fillable = [
+        'children_name',
+        'birth_date',
+        'parent_name',
+        'address',
+        'phone_number',
+        'email',
+        'weight',
+        'height',
+        'gender',
+        'recorded_by'
+    ];
+}
---
+++ resources/js/Pages/Worker/Record/RecordIndex.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import WorkerLayout from "@/Layouts/WorkerLayout.jsx";
+
+const RecordIndex = () => {
+    return (
+        <WorkerLayout>
+            hi
+        </WorkerLayout>
+    )
+}
+
+export default RecordIndex
