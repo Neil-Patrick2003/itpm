59c5b73c18b06898710b1b97ad85cb6aebe13973
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-04-30 00:28:49 +0800

    add - empty estate

--- resources/js/Pages/Admin/Program/Show.jsx
+++ resources/js/Pages/Admin/Program/Show.jsx
@@ -11,11 +11,12 @@ import {FaEdit} from "react-icons/fa";
 import {GrFormView} from "react-icons/gr";
 import {MdDeleteForever} from "react-icons/md";
 import children from "@/Pages/Admin/Children/Children.jsx";
+import EmptyState from "@/Components/EmptyState.jsx";
+import {motion} from "framer-motion";
 
 
 const Show = ({ program, beneficiaries }) => {
 
-    console.log(program.sponsor);
     // Function to calculate age from the birthdate
     const calculateAge = (birthdate) => {
         const birthDate = new Date(birthdate);
@@ -34,10 +35,7 @@ const Show = ({ program, beneficiaries }) => {
 
 
     // Handle edit action (currently just logs the row, can be expanded)
-    const handleEdit = (row) => {
-        console.log('Edit clicked for: ', row);
-        // Handle the edit functionality, such as    opening a modal or redirecting
-    };
+
 
     // Handle delete action with SweetAlert confirmation
     const handleDelete = (id) => {
@@ -175,20 +173,8 @@ const Show = ({ program, beneficiaries }) => {
                 <div className="bg-white mt-4 py-8 px-4 space-y-4 rounded ">
                     <h1 className="text-2xl font-semibold text-gray-800">{program.title}</h1>
                     <p className="text-gray-600">{program.description}</p>
-                    <h3 className="text-xl font-semibold text-gray-800">Sponsor</h3>
-                    <ul>
-                        {/*{program.sponsor.length === 0 ? (*/}
-                        {/*    <li>No sponsor found</li>*/}
-                        {/*): (*/}
-                        {/*    program.sponsor.data.map((sponsor) => (*/}
-                        {/*        <li key={sponsor.id} className="flex items-center gap-2"></li>*/}
-                        {/*    ))*/}
-                        {/*)*/}
-                        {/*}*/}
-
-                    </ul>
                 </div>
-                <div className='flex justify-center my-4' >
+                <div className='flex justify-center my-4'>
                     <Link href={`/programs/${program.id}/add_beneficiaries`}>
                         <button className="flex gap-2 bg-[#01DAA2] rounded-full px-4 py-2 text-white hover:bg-green-400">
                             <span>
@@ -198,26 +184,44 @@ const Show = ({ program, beneficiaries }) => {
                         </button>
                     </Link>
                 </div>
-                <div className="overflow-auto rounded-md "    >
+                <div className="overflow-auto rounded-md">
                     <table className="min-w-full divide-y divide-gray-200">
                         <thead className="bg-green-100">
                         <tr>
-                            <th className="px-6 py-3 text-left text-xs font-bold text-green-700 uppercase tracking-wider">Id</th>
-                            <th className="px-6 py-3 text-left text-xs font-bold text-green-700 uppercase tracking-wider">Name</th>
-                            <th className="px-6 py-3 text-left text-xs font-bold text-green-700 uppercase tracking-wider">Age</th>
-                            <th className="px-6 py-3 text-left text-xs font-bold text-green-700 uppercase tracking-wider">Gender</th>
-                            <th className="px-6 py-3 text-left text-xs font-bold text-green-700 uppercase tracking-wider">Status</th>
-                            <th className="px-6 py-3 text-center text-xs font-bold text-green-700 uppercase tracking-wider">Actions</th>
+                            <th className="px-6 py-3 text-left text-xs font-bold text-green-700 uppercase tracking-wider" scope="col">Id</th>
+                            <th className="px-6 py-3 text-left text-xs font-bold text-green-700 uppercase tracking-wider" scope="col">Name</th>
+                            <th className="px-6 py-3 text-left text-xs font-bold text-green-700 uppercase tracking-wider" scope="col">Age</th>
+                            <th className="px-6 py-3 text-left text-xs font-bold text-green-700 uppercase tracking-wider" scope="col">Gender</th>
+                            <th className="px-6 py-3 text-left text-xs font-bold text-green-700 uppercase tracking-wider" scope="col">Status</th>
+                            <th className="px-6 py-3 text-center text-xs font-bold text-green-700 uppercase tracking-wider" scope="col">Remarks</th>
                         </tr>
                         </thead>
-                        <tbody className="divide-y divide-gray-200 bg-white">
+
+                        <tbody className="divide-y divide-gray-200 bg-white w-full">
                         {beneficiaries.length === 0 ? (
                             <tr>
-                                <td colSpan="5" className="text-center py-4 text-gray-500">No beneficiaries found.</td>
+                                <td colSpan="6" className="text-center p-4 text-gray-500">
+                                    <motion.div
+                                        initial={{ opacity: 0, y: 20 }}
+                                        animate={{ opacity: 1, y: 0 }}
+                                        transition={{ duration: 0.4 }}
+                                        className="flex flex-col items-center justify-center text-center p-8 border border-dashed border-gray-300 rounded-md bg-white text-gray-500"
+                                    >
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                             stroke-width="1.5" stroke="currentColor" className="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                  d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"/>
+                                        </svg>
+
+                                        <p className="text-lg font-medium">No Beneficiary yet</p>
+                                        <p className="text-sm text-gray-400 mt-1">Add new beneficiaries and start improving health</p>
+                                    </motion.div>
+                                </td>
                             </tr>
                         ) : (
-                             beneficiaries.map((beneficiary) => (
-                                    <tr key={beneficiaries.length}>
+                            beneficiaries.map((beneficiary) => (
+                                beneficiary.children ? (
+                                    <tr key={beneficiary.children.id}>
                                         <td className="px-6 py-4 text-sm text-gray-700">{beneficiary.children.id}</td>
                                         <td className="px-6 py-4 text-sm text-gray-900">
                                             <div className="flex items-center gap-3">
@@ -229,35 +233,22 @@ const Show = ({ program, beneficiaries }) => {
                                         <td className="px-6 py-4 text-sm text-gray-600">
                                             <div>{calculateAge(beneficiary.children.birth_date)} years old</div>
                                         </td>
-
                                         <td className="px-6 py-4 text-sm text-gray-700">{beneficiary.children.gender}</td>
                                         <td className="px-6 py-4 text-sm text-gray-600">
-                                            {beneficiary.children.record.map((record) => (
-                                               <li key={record.id} className="flex items-center gap-2">
-                                                   <p>{
-                                                       record.bmi < 18.5 ? 'Underweight' :
-                                                       record.bmi < 24.9 ? 'Normal' :
-                                                       record.bmi < 29.9 ? 'Overweight' :
-                                                       'Obese'
-                                                   }
-                                                   </p>
-                                               </li>
-                                            ))}
+                                            {/* You can handle the status logic here */}
                                         </td>
                                         <td className="px-6 py-4 flex justify-center gap-3 text-gray-600">
-                                            Remarks
-                                            {/*<button className="flex items-center gap-1 px-3 py-1 rounded-md bg-red-400 text-white hover:bg-red-500" onClick={() => handleDelete(beneficiary.children.id)}*/}
-                                            {/*>*/}
-                                            {/*    <MdDeleteForever className="w-4 h-4" /> Remove*/}
-                                            {/*</button>*/}
+                                            {/* Render remarks or any other data */}
+                                            -Remark-
                                         </td>
                                     </tr>
-                                ))
+                                ) : null
+                            ))
                         )}
-
                         </tbody>
                     </table>
                 </div>
+
                 {/*<div className='mt-4'>*/}
                 {/*    <Paper sx={{ height: 800, width: '100%' }}>*/}
                 {/*        <DataGrid*/}
