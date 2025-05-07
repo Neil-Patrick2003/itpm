d56c7b25312791fe5e1cdbe8adb6362c5d108417
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-05-08 00:20:43 +0800

    register pWA

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
     <head>
+        @PwaHead <!-- Add this directive to include the PWA meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -25,5 +26,6 @@
 {{--        };--}}
 {{--    </script>--}}
         @inertia
+        @RegisterServiceWorkerScript
     </body>
 </html>
