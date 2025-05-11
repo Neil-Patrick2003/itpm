f3c5df8029443c31e4d2636ad091442d9ae258b2
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-05-12 05:01:14 +0800

    wip

--- app/Http/Controllers/API/Parent/ForumController.php
+++ app/Http/Controllers/API/Parent/ForumController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API\Parent;
 use App\Http\Controllers\Controller;
 use App\Models\Children;
 use App\Models\Topic;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 
 class ForumController extends Controller
