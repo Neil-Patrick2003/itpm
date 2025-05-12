acc6c0e5ae5c8be49b0a81be7dbc6ebd9742c050
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-05-12 18:55:08 +0800

    Post api

---
+++ app/Http/Controllers/API/Parent/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\API\Parent;
+
+use App\Http\Controllers\Controller;
+use App\Models\Children;
+use App\Models\Post;
+use App\Models\Topic;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class PostController extends Controller
+{
+    public function __invoke(Request $request, $id)
+    {
+        $request->validate([
+            'body' => 'required'
+        ]);
+
+        Post::create(['topic_id' => $id, 'body' => $request->body, 'user_id' => auth()->id()]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\API\Parent\BlogController;
 use App\Http\Controllers\API\Parent\ChildrenController;
 use App\Http\Controllers\API\Parent\ForumController;
 use App\Http\Controllers\API\Parent\LoginController;
+use App\Http\Controllers\API\Parent\PostController;
 use App\Models\Blog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,7 +17,9 @@ Route::group(['middleware' => 'auth:sanctum', 'prefix' => '/parent'], function (
     Route::get('/announcements', AnnouncementController::class);
     Route::get('/forum', [ForumController::class, 'index']);
     Route::post('/forum', [ForumController::class, 'store']);
-    Route::get('/blog', [BlogController::class, 'index']);
+    Route::post('/forum/{id}/post', PostController::class);
+
+    Route::get('/blog', action: [BlogController::class, 'index']);
 });
 
 
