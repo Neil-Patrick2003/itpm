89a6d31a518c5e0560018ee6ed3be40c7a252370
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2025-05-12 22:54:38 +0800

    nutrition api

---
+++ app/Http/Controllers/API/Parent/NutritionController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\API\Parent;
+
+use App\Http\Controllers\Controller;
+
+class NutritionController extends Controller{
+    public function index()
+    {
+        //
+    }
+}
