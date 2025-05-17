6ba6158c9b3772d1c6ebbb13822d11312ab812df
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2025-05-17 19:43:56 +0800

    wip

--- app/Http/Controllers/API/Parent/AnnouncementController.php
+++ app/Http/Controllers/API/Parent/AnnouncementController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\API\Parent;
 
 use App\Http\Controllers\Controller;
 use App\Models\Announcement;
+use App\Models\Program;
+use Carbon\Carbon;
+
 
 class AnnouncementController extends Controller
 {
     public function __invoke()
     {
+        
+
+        $incoming_events = Program::where('start_date', '>', Carbon::now())->get();
+
         $announcements = Announcement::get();
 
         return [
